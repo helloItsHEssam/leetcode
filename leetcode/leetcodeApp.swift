c00bfe6e7a9139d16957cb9ae4533a5be749e76e
Hessam Mahdiabadi <67460597+iamHEssam@users.noreply.github.com>
2023-09-27 12:03:09 +0330

    Initial Commit

---
+++ leetcode/leetcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  leetcodeApp.swift
+//  leetcode
+//
+//  Created by Hessam Mahdiabadi on 9/27/23.
+//
+
+import SwiftUI
+
+@main
+struct leetcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
